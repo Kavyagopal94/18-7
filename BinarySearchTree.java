5c74ecba788e9404a23d9b5dbdbe1ee67614fe7a
Kavya.G <90204388+Kavyagopal94@users.noreply.github.com>
2024-07-18 14:34:05 +0530

    Create BinarySearchTree.java

---
+++ BinarySearchTree.java
@@ -0,0 +1,63 @@
+public class BinarySearchTree<T extends Comparable<T>> {
+
+    Node<T> root;
+
+    public BinarySearchTree() {
+        root = null;
+    }
+
+    public void insert(T data) {
+        root = insertRec(root, data);
+    }
+
+    private Node<T> insertRec(Node<T> root, T data) {
+        if (root == null) {
+            root = new Node<T>(data);
+            return root;
+        }
+
+        if (data.compareTo(root.data) < 0)
+            root.left = insertRec(root.left, data);
+        else if (data.compareTo(root.data) > 0)
+            root.right = insertRec(root.right, data);
+
+        return root;
+    }
+
+    public void inorder() {
+        inorderRec(root);
+    }
+
+    private void inorderRec(Node<T> root) {
+        if (root != null) {
+            inorderRec(root.left);
+            System.out.print(root.data + " ");
+            inorderRec(root.right);
+        }
+    }
+
+    public void preorder() {
+        preorderRec(root);
+    }
+
+    private void preorderRec(Node<T> root) {
+        if (root != null) {
+            System.out.print(root.data + " ");
+            preorderRec(root.left);
+            preorderRec(root.right);
+        }
+    }
+
+    public void postorder() {
+        postorderRec(root);
+    }
+
+    private void postorderRec(Node<T> root) {
+        if (root != null) {
+            postorderRec(root.left);
+            postorderRec(root.right);
+            System.out.print(root.data + " ");
+        }
+    }
+
+}
